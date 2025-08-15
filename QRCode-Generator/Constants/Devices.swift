c798f9991e141401f3b2abb70986ed522de856ab
DaweiHao <daweihao84@icloud.com>
2025-08-15 10:48:35 +0800

    Complete the Devices for testing.

--- QRCode-Generator/Constants/Devices.swift
+++ QRCode-Generator/Constants/Devices.swift
@@ -10,14 +10,39 @@ import SwiftUI
 
 extension PreviewDevice {
     // MARK: - iPhone
-    static let iPhone16         = PreviewDevice(rawValue: "iPhone 16")
-    static let iPhone16Pro      = PreviewDevice(rawValue: "iPhone 16 Pro")
-    static let iPhone15ProMax   = PreviewDevice(rawValue: "iPhone 15 Pro Max")
-    static let iPhoneSE3        = PreviewDevice(rawValue: "iPhone SE (3rd generation)")
-    
+    static let iPhone16             = PreviewDevice(rawValue: "iPhone 16")
+    static let iPhone16Plus         = PreviewDevice(rawValue: "iPhone 16 Plus")
+    static let iPhone16Pro          = PreviewDevice(rawValue: "iPhone 16 Pro")
+    static let iPhone16ProMax       = PreviewDevice(rawValue: "iPhone 16 Pro Max")
+    static let iPhone15             = PreviewDevice(rawValue: "iPhone 15")
+    static let iPhone15Plus         = PreviewDevice(rawValue: "iPhone 15 Plus")
+    static let iPhone15Pro          = PreviewDevice(rawValue: "iPhone 15 Pro")
+    static let iPhone15ProMax       = PreviewDevice(rawValue: "iPhone 15 Pro Max")
+    static let iPhone14             = PreviewDevice(rawValue: "iPhone 14")
+    static let iPhone14Plus         = PreviewDevice(rawValue: "iPhone 14 Plus")
+    static let iPhone14Pro          = PreviewDevice(rawValue: "iPhone 14 Pro")
+    static let iPhone14ProMax       = PreviewDevice(rawValue: "iPhone 14 Pro Max")
+    static let iPhone13             = PreviewDevice(rawValue: "iPhone 13")
+    static let iPhone13Mini         = PreviewDevice(rawValue: "iPhone 13 mini")
+    static let iPhone13Pro          = PreviewDevice(rawValue: "iPhone 13 Pro")
+    static let iPhone13ProMax       = PreviewDevice(rawValue: "iPhone 13 Pro Max")
+    static let iPhoneSE3            = PreviewDevice(rawValue: "iPhone SE (3rd generation)")
+    static let iPhoneSE2            = PreviewDevice(rawValue: "iPhone SE (2nd generation)")
+
     // MARK: - iPad
-    static let iPadPro129Gen5   = PreviewDevice(rawValue: "iPad Pro 12.9-inch (5th generation)")
-    static let iPadPro11M4      = PreviewDevice(rawValue: "iPad Pro 11-inch (M4)")
-    static let iPadMini6        = PreviewDevice(rawValue: "iPad mini (6th generation)")
+    static let iPadPro129Gen5       = PreviewDevice(rawValue: "iPad Pro 12.9-inch (5th generation)")
+    static let iPadPro129Gen6       = PreviewDevice(rawValue: "iPad Pro 12.9-inch (6th generation)")
+    static let iPadPro11Gen4        = PreviewDevice(rawValue: "iPad Pro 11-inch (4th generation)")
+    static let iPadPro11M4          = PreviewDevice(rawValue: "iPad Pro 11-inch (M4)")
+    static let iPadAir5             = PreviewDevice(rawValue: "iPad Air (5th generation)")
+    static let iPadAirM2            = PreviewDevice(rawValue: "iPad Air 11-inch (M2)")
+    static let iPadMini6            = PreviewDevice(rawValue: "iPad mini (6th generation)")
+    static let iPad10Gen            = PreviewDevice(rawValue: "iPad (10th generation)")
+
+    // MARK: - Mac
+    static let macStudioDisplay     = PreviewDevice(rawValue: "Mac Studio Display")
+    static let macBookAirM2         = PreviewDevice(rawValue: "MacBook Air (M2)")
+    static let macBookPro14M3       = PreviewDevice(rawValue: "MacBook Pro 14-inch (M3)")
 }
+
 
